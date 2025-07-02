b2201040e909023b026c6fdda814f85402dce95d
kristiangarcia <kristiangarciapaulsen@gmail.com>
2025-07-02 19:19:10 +0200

    Release v0.0.9-alpha.1 (pre-release)

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -29,7 +29,7 @@ const Sidebar: React.FC<SidebarProps> = ({ activeSection, onSectionChange }) => 
   const hasUpdate = false;
   
   // Version is automatically updated by release.js
-  const currentVersion = "0.0.8-alpha.5";
+  const currentVersion = "0.0.9-alpha.1";
 
   const menuItems = [
     {
