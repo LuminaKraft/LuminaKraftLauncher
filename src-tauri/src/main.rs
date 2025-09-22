2c0a4bb69df0803eceb13ba5e52956cbb3787608
kristiangarcia <kristiangarciapaulsen@gmail.com>
2025-09-23 00:00:59 +0200

    feat: add HTTP plugin support in Tauri configuration
    - Include `tauri-plugin-http` in the Cargo.toml dependencies.
    - Enable HTTP plugin in tauri.conf.json with specified API scope.
    - Initialize HTTP plugin in main.rs to enhance network capabilities.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -861,6 +861,7 @@ fn main() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_http::init())
         .invoke_handler(tauri::generate_handler![
             get_instance_metadata,
             install_modpack,
