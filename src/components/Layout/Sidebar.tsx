9f4108cb88749273f094506f5328444aaa5f9fb6
kristiangarcia <kristiangarciapaulsen@gmail.com>
2026-01-14 03:12:12 +0100

    Release v0.1.6

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -60,7 +60,7 @@ const Sidebar: React.FC<SidebarProps> = ({ activeSection, onSectionChange }) => 
   }, [userSettings?.enablePrereleases]);
 
   // Version is automatically updated by release.js
-  const currentVersion = "0.1.5";
+  const currentVersion = "0.1.6";
 
   const menuItems = [
     {
