1e73b3c0bb46710d8d164f3d4cafda09b0de05c8
Kristian García <kristiangarciapaulsen@gmail.com>
2026-05-05 23:33:21 +0200

    chore: remove leftover test_db files

--- test_db.js
+++
@@ -1,7 +0,0 @@
-import { supabase } from './src/services/supabaseClient.js';
-async function run() {
-  const { data, error } = await supabase.from('modpacks').select('*').limit(1);
-  if (error) console.error(error);
-  console.log(Object.keys(data[0] || {}));
-}
-run();
